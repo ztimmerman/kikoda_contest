a18812596f222bb73bfc60c5a84d953bdb21d923
Md Mainuddin <mmainudd@gmail.com>
2018-02-02 18:40:21 -0500

    basic ajax implementation for search

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function () {
+    var params = new Object();
+    params.api_key = "05875cd50919223ef7db595c5c0743c4";
+    params.page = 1;
+    $.ajax({
+        dataType: "json",
+        method: "GET",
+        url: "https://api.themoviedb.org/3/discover/movie",
+        data: params
+    }).done(function (result) {
+        console.log(result);
+    });
+});
